bed9c6e29a39e4949309801890828d2fa6cf6e24
Sateshi <sateshiplay@gmail.com>
2021-12-13 14:01:20 +0100

    EXO 2 FINI JB

---
+++ DOM_EXO2/main.js
@@ -0,0 +1,12 @@
+let myBody = document.body;
+console.log(myBody)
+console.log(myBody.firstElementChild)
+console.log(myBody.lastElementChild)
+
+let exo4 = document.querySelector("div").children;
+console.log(exo4)
+
+let secondLi = document.querySelector("li").nextElementSibling;
+console.log(secondLi)
+
+console.log(secondLi.previousElementSibling)
