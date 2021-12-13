8987d3c65de6a4168c7f7156f9be5649c3861a13
Sateshi <sateshiplay@gmail.com>
2021-12-13 12:24:43 +0100

    Exo 1 DOM JB

---
+++ DOM_EXO1/main.js
@@ -0,0 +1,11 @@
+let h1 = document.getElementsByTagName("h1");
+console.log(h1);
+
+let h3 = document.getElementsByTagName("h3");
+console.log(h3);
+
+let p =  document.getElementsByTagName("p");
+console.log(p[1]);
+
+let li = document.getElementsByTagName("li")[2];
+console.log(li);
