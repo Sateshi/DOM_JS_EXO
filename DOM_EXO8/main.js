90693bdb5742e5aa919cf931b9d484ee884130de
Sateshi <sateshiplay@gmail.com>
2021-12-14 12:24:21 +0100

    exo8 fini jb

---
+++ DOM_EXO8/main.js
@@ -0,0 +1,18 @@
+let redPurple =  document.getElementsByClassName("redPurple")
+console.log(redPurple)
+
+let redPurpleQuery = document.querySelectorAll(".redPurple")
+console.log(redPurpleQuery)
+
+let h1 =  document.querySelectorAll("h1.redPurple")
+console.log(h1);
+
+let all = document.querySelectorAll("li, p, span")
+console.log(all)
+
+let all2 = document.querySelectorAll("li.important, p")
+console.log(all2)
+
+let all3 = document.querySelectorAll("h1.redPurple, span.redPurple")
+console.log(all3)
+
