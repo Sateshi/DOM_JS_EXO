cf46dcc460fe765537d139b93b85a6139ad0d463
Sateshi <sateshiplay@gmail.com>
2021-12-14 11:17:21 +0100

    exo 6 jb

---
+++ DOM_EXO5/main.js
@@ -0,0 +1,6 @@
+let tartine = document.getElementsByClassName("tartine")[0].innerHTML;
+console.log(tartine);
+let toast = document.getElementById("toast").innerHTML;
+console.log(toast);
+let dernier = document.getElementsByClassName("tartine")[document.getElementsByClassName("tartine").length - 1];
+console.log(dernier)
